b30d53bff05115f86524648373c3f2e9b4cb4412
Anthcode <108927171+Anthcode@users.noreply.github.com>
2023-05-07 16:56:29 +0200

    2 files modified , login form style

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,7 +2,12 @@ import React from 'react';
 export default function Login() {
   return (
     <div className="login">
-      <h1>Login</h1>
+      <form>
+        <h2>Login</h2>
+        <input type="email" placeholder="your@email.com" />
+        <input type="password" placeholder="passwd" />
+        <button className="btn-login">Log in</button>
+      </form>
     </div>
   );
 }
