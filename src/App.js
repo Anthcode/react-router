744c7e21dab437e91841e69b5fa6615e39822ce0
Anthcode <108927171+Anthcode@users.noreply.github.com>
2023-05-07 15:33:51 +0200

    2 files modified, 1 added routes create

--- src/App.js
+++ src/App.js
@@ -5,15 +5,17 @@ import Layout from './components/Layout';
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
+import Login from './components/Login';
 
 export default function App() {
   return (
     <div className="app">
       <Routes>
         <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="about" element={<About />} />
-            <Route path="contact" element={<Contact />} />
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="login" element={<Login />} />
         </Route>
       </Routes>
     </div>
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -2,23 +2,36 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 export default function Menu() {
-  
   const activeStyles = {
-
-    color: "lightgreen"
-}
+    color: 'lightgreen',
+  };
   return (
     <div className="menu">
       <nav>
-          <NavLink to="/" style={({isActive}) => isActive ? activeStyles : null}>
-            Home
-          </NavLink>
-          <NavLink to="about" style={({isActive}) => isActive ? activeStyles : null}>
-            About
-          </NavLink>
-          <NavLink to="contact" style={({isActive}) => isActive ? activeStyles : null}>
-            Contact
-          </NavLink>
+        <NavLink
+          to="/"
+          style={({ isActive }) => (isActive ? activeStyles : null)}
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="about"
+          style={({ isActive }) => (isActive ? activeStyles : null)}
+        >
+          About
+        </NavLink>
+        <NavLink
+          to="contact"
+          style={({ isActive }) => (isActive ? activeStyles : null)}
+        >
+          Contact
+        </NavLink>
+        <NavLink
+          to="login"
+          style={({ isActive }) => (isActive ? activeStyles : null)}
+        >
+          Login
+        </NavLink>
       </nav>
     </div>
   );
