4a698ba4c349caecb48edffa49e34c72e9c74ac6
Anthcode <108927171+Anthcode@users.noreply.github.com>
2023-05-07 18:28:57 +0200

    2 files modified, add login and passwd state

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 export default function Login() {
+  const [login, setLogin] = useState({ email: '', passwd: '' });
+
+  const signIn = (e) => {
+    const { name, value } = e.target;
+    setLogin({...login, [name]:value})
+    console.log(login);
+  };
   return (
     <div className="login">
       <form>
         <h2>Login</h2>
-        <input type="email" placeholder="your@email.com" />
-        <input type="password" placeholder="passwd" />
+        <input
+          type="email"
+          placeholder="your@email.com"
+          name="email"
+          onChange={signIn}
+         
+        />
+        <h3> {login.email} </h3>
+        <input
+          type="password"
+          placeholder="passwd"
+          name="passwd"
+          onChange={signIn}
+         
+        />
         <button className="btn-login">Log in</button>
+       
       </form>
+   
+       
     </div>
   );
 }
