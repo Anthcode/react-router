2d220e045c1b8b2392d51c156ebbc414f8e9e82b
Anthcode <108927171+Anthcode@users.noreply.github.com>
2023-05-09 11:13:05 +0200

    1 file modified, submit logina data object

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,34 +1,33 @@
 import React, { useState } from 'react';
 export default function Login() {
-  const [login, setLogin] = useState({ email: '', passwd: '' });
+  const [login, setLogin] = useState({});
 
   const signIn = (e) => {
-    const { name, value } = e.target;
-    setLogin({...login, [name]:value})
-    console.log(login);
+    e.preventDefault();
+   const data = new FormData(e.target)
+   let formObject = Object.fromEntries(data.entries())
+   console.log(formObject)
+   setLogin(formObject)
   };
   return (
     <div className="login">
-      <form>
+      <form onSubmit={signIn}>
         <h2>Login</h2>
         <input
           type="email"
           placeholder="your@email.com"
           name="email"
-          onChange={signIn}
-         
+         // onChange={signIn}
         />
-        
         <input
           type="password"
           placeholder="passwd"
           name="passwd"
-          onChange={signIn}
-         
+         // onChange={signIn}
         />
-        <button className="btn-login">Log in</button>
-      <h3> {login.email} </h3>
-      <h3> {login.passwd} </h3>
+        <button className="btn-login" type="submit">Log in</button>
+      <h3>{login.email}</h3>
+      <h3>{login.passwd}</h3>
       </form>
    
       
