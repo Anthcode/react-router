e5bcf3f77014017cd78d6fbf69790e57b52f39d0
Anthcode <108927171+Anthcode@users.noreply.github.com>
2023-05-16 09:41:13 +0200

    add env_variable

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,10 +4,10 @@ export default function Login() {
 
   const signIn = (e) => {
     e.preventDefault();
-   const data = new FormData(e.target)
-   let formObject = Object.fromEntries(data.entries())
-   console.log(formObject)
-   setLogin(formObject)
+    const data = new FormData(e.target);
+    let formObject = Object.fromEntries(data.entries());
+    console.log(formObject);
+    setLogin(formObject);
   };
   return (
     <div className="login">
@@ -17,20 +17,21 @@ export default function Login() {
           type="email"
           placeholder="your@email.com"
           name="email"
-         // onChange={signIn}
+          // onChange={signIn}
         />
         <input
           type="password"
           placeholder="passwd"
           name="passwd"
-         // onChange={signIn}
+          // onChange={signIn}
         />
-        <button className="btn-login" type="submit">Log in</button>
-      <h3>{login.email}</h3>
-      <h3>{login.passwd}</h3>
+        <button className="btn-login" type="submit">
+          Log in
+        </button>
+        <h3>{login.email}</h3>
+        <h3>{login.passwd}</h3>
+        {process.env.REACT_APP_NAME}
       </form>
-   
-      
     </div>
   );
 }
