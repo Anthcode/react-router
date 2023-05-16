22cc8d61c153f8be06b3a610e520a6322cbb42ec
Anthcode <108927171+Anthcode@users.noreply.github.com>
2023-05-16 11:01:27 +0200

    firebase config + add package

---
+++ src/firebase/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBHOdn1gsYUBwxZGb-mcq-T4OSHmhaTkBU',
+
+  authDomain: 'auth-644f4.firebaseapp.com',
+
+  projectId: 'auth-644f4',
+
+  storageBucket: 'auth-644f4.appspot.com',
+
+  messagingSenderId: '335012110278',
+
+  appId: '1:335012110278:web:05ef16ae90d6c35705dcc7',
+};
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
