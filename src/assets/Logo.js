07f80353e16834ec6ebcaf7a7a947b303ee4be15
Anthcode <108927171+Anthcode@users.noreply.github.com>
2023-05-16 10:04:08 +0200

    clean code

--- src/assets/Logo.js
+++ src/assets/Logo.js
@@ -3,7 +3,7 @@ const logo = 'https://cdn.worldvectorlogo.com/logos/react-2.svg';
 
 export default function Logo() {
   return (
-    <div c>
+    <div>
       <img src={logo} alt="logo" className="logo" />
     </div>
   );
