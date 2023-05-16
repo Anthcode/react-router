4a0338971c439cca23b6dfe11dd1a547ee791036
Anthcode <108927171+Anthcode@users.noreply.github.com>
2023-05-16 14:14:47 +0200

    1 file modified

--- src/components/Login.js
+++ src/components/Login.js
@@ -10,18 +10,23 @@ export default function Login() {
   const navigate = useNavigate();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [isLoging, setIsLoging] = useState(false);
 
   const logIn = async (e) => {
+    setIsLoging(true);
     e.preventDefault();
+
     await signInWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
         const user = userCredential.user;
-        navigate("/");
+        navigate('/');
+        setIsLoging(false);
         console.log(user);
       })
       .catch((error) => {
         const errorCode = error.code;
         const errorMessage = error.message;
+        setIsLoging(false);
         console.log(errorCode, errorMessage);
       });
   };
@@ -44,21 +49,22 @@ export default function Login() {
       <form>
         <h2>Login</h2>
         <div className="input-div">
-        <input
-          type="email"
-          placeholder="your@email.com"
-          name="email"
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="passwd"
-          name="passwd"
-          onChange={(e) => setPassword(e.target.value)}
-        />
+          <input
+            type="email"
+            placeholder="your@email.com"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="passwd"
+            name="passwd"
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </div>
-        <button className="btn-login" onClick={logIn}>
-          Log in
+        <button className="btn-login" onClick={logIn} disabled={!email}>
+          Login
         </button>
         <button className="btn-login" onClick={createUser}>
           Register
