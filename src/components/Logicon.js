344f320e992ec7febc26202008e55b7cbf6668cf
Anthcode <108927171+Anthcode@users.noreply.github.com>
2023-05-17 09:02:25 +0200

    1 file modified

--- src/components/Logicon.js
+++ src/components/Logicon.js
@@ -1,27 +1,20 @@
 import React, { useContext } from 'react';
-import { useNavigate } from 'react-router-dom';
-import {  signOut } from "firebase/auth";
-import { auth } from '../firebase/firebase';
 import AuthContext from "../auth/AuthContext";
 
 export default function Logicon() {
-//const [isLogged, setIsLogged] = useState(true)
+
 const user = useContext(AuthContext)
 
   return (
-    <>
-
         <div>
-          {user ? (<>
+          {user ?(
+          <>
             <p>Użytkownik jest zalogowany jako {user.email}</p>
             <button onClick={logOut}> Logout </button>
-            </>
+          </>
           ) : (
             <p>Użytkownik nie jest zalogowany</p>
           )}
         </div>
-
-   </>
-    
   );
 }
