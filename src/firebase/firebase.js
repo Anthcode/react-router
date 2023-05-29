fd6ae0178207e430030f19a9d76300ce96e3f85e
Anthcode <108927171+Anthcode@users.noreply.github.com>
2023-05-29 08:08:08 +0200

    add databse config

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
+import { getDatabase } from 'firebase/database'
 
 const firebaseConfig = {
   apiKey: 'AIzaSyBHOdn1gsYUBwxZGb-mcq-T4OSHmhaTkBU',
@@ -20,4 +21,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
+const db = getDatabase(app);
 export default app;
