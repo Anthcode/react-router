256310af50f0f23dcfdaaf69ab6e60f8d56e193e
Anthcode <108927171+Anthcode@users.noreply.github.com>
2023-06-05 14:32:35 +0200

    1 file modified

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,26 +1,25 @@
 import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-import { getDatabase } from 'firebase/database'
+import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: "",
+  apiKey: 'AIzaSyBHOdn1gsYUBwxZGb-mcq-T4OSHmhaTkBU',
 
-  authDomain: "auth-644f4.firebaseapp.com",
+  authDomain: 'auth-644f4.firebaseapp.com',
 
-  databaseURL: "https://auth-644f4-default-rtdb.europe-west1.firebasedatabase.app",
+  databaseURL: 'https://auth-644f4-default-rtdb.europe-west1.firebasedatabase.app',
 
-  projectId: "auth-644f4",
+  projectId: 'auth-644f4',
 
-  storageBucket: "auth-644f4.appspot.com",
+  storageBucket: 'auth-644f4.appspot.com',
 
-  messagingSenderId: "335012110278",
+  messagingSenderId: '335012110278',
 
-  appId: "1:335012110278:web:05ef16ae90d6c35705dcc7"
-
+  appId: '1:335012110278:web:05ef16ae90d6c35705dcc7',
 };
 
 // Initialize Firebase
 const app = getApps.Length > 0 ? getApps() : initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getDatabase(app);
-export {app , auth, db};
+export { app, auth, db };
